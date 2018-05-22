c76f67a4de8cceb28ef2dc2b38b276e2ed85c687
davidromesp <32366797+davidromesp@users.noreply.github.com>
2018-05-22 17:12:34 +0200

    Category Junit tests finished
    Added delete category, and some fixes

--- Acme-Patronage/src/test/java/services/CategoryServiceTest.java
+++ Acme-Patronage/src/test/java/services/CategoryServiceTest.java
@@ -33,7 +33,7 @@ public class CategoryServiceTest extends AbstractTest {
 	 * category is in use by any project it cannot be edited, only deleted. This
 	 * removes the category from the project.
 	 * 
-	 * Test create category
+	 * Tests-Create a category
 	 * Positive test1: An admin creates a category, with a name and a description
 	 * Negative test2: An admin creates a category, with a null name and a description
 	 * Negative test3: An admin creates a category, with a name and a null description
@@ -53,10 +53,9 @@ public class CategoryServiceTest extends AbstractTest {
 			}, {
 				"admin", "Nameofcategory", null, ConstraintViolationException.class
 			}, {
-				"user2", "Name233", "Description", ConstraintViolationException.class
+				"user2", "Name233", "Description2", ConstraintViolationException.class
 			}, {
-				"admin", "Games", "Description", ConstraintViolationException.class
-
+				"admin", "Art", "Description", ConstraintViolationException.class
 			}, {
 				"Corporation1", "Name3", "Description", ConstraintViolationException.class
 			}, {
@@ -97,14 +96,13 @@ public class CategoryServiceTest extends AbstractTest {
 	}
 
 	/**
-	 * Acme-Patronage: Requirement 12 An actor who is authenticated as an administrator must be able to:
-	 * 12.1. Manage the categories, thus create, edit and delete a category. If a
-	 * category is in use by any project it cannot be edited, only deleted. This
-	 * removes the category from the project.
+	 * Acme-Patronage: Requirement 12
+	 * 
+	 * Tests- Save a category
 	 * 
 	 * Positive test1: An admin modifies a category that does not belong to a project
 	 * Negative test2: An admin modifies a category that belongs to a project
-	 * Negative test3: An admin creates a category, with a name already in use
+	 * Negative test3: An admin modifies a category,but the name is already in use
 	 * Negative test4: A user tries to modify a category.
 	 * Negative test5: A unauthenticated user tries to modify a category
 	 * Negative test6: A corporation tries to modify a category
@@ -116,6 +114,7 @@ public class CategoryServiceTest extends AbstractTest {
 
 		final Category c1 = this.categoryService.findOne(this.getEntityId("category1"));
 		//c1 belongs to a project(not editable)
+
 		final Category c2 = this.categoryService.create();
 		c2.setDescription("Description2");
 		c2.setName("Name2");
@@ -128,7 +127,7 @@ public class CategoryServiceTest extends AbstractTest {
 			}, {
 				"admin", c1, "Name1", "description", IllegalArgumentException.class
 			}, {
-				"admin", c2, "Games", "description", null
+				"admin", c2, "Art", "description", IllegalArgumentException.class
 			}, {
 				"user2", c2, "Name2", "Description", IllegalArgumentException.class
 			}, {
@@ -156,6 +155,70 @@ public class CategoryServiceTest extends AbstractTest {
 			category.setDescription(description);
 
 			this.categoryService.saveFromEdit(category);
+			this.unauthenticate();
+			this.categoryService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 12
+	 * 
+	 * Tests- Delete a category
+	 * 
+	 * Positive test1: An admin deletes a category that does not belong to a project
+	 * Negative test2: An admin deletes a category that belongs to a project
+	 * Negative test3: An admin creates a category, with a name already in use
+	 * Negative test4: A user tries to delete a category.
+	 * Negative test5: A unauthenticated user tries to delete a category
+	 * Negative test6: A corporation tries to delete a category
+	 * 
+	 */
+	@Test
+	public void testDeleteCategoryDriver() {
+
+		final Category c1 = this.categoryService.findOne(this.getEntityId("category1"));
+		//c1 belongs to a project(not editable nor deletable)
+		final Category c2 = this.categoryService.create();
+		c2.setDescription("Description2");
+		c2.setName("Name2");
+
+		final Object testingData[][] = {
+
+			/** userPrincipal, category,exception */
+			{
+				"admin", c2, null
+			}, {
+				"admin", c1, IllegalArgumentException.class
+			}, {
+				"user2", c2, IllegalArgumentException.class
+			}, {
+				null, c2, IllegalArgumentException.class
+			}, {
+				"Corporation1", c2, IllegalArgumentException.class
+			},
+
+		};
+
+		for (int i = 0; i < testingData.length; i++) {
+			this.setUp();
+			this.testDeleteCategoryTemplate((String) testingData[i][0], (Category) testingData[i][1], (Class<?>) testingData[i][2]);
+			this.tearDown();
+		}
+	}
+	protected void testDeleteCategoryTemplate(final String username, final Category category, final Class<?> expectedException) {
+		Class<?> caught;
+		caught = null;
+
+		try {
+			this.authenticate(username);
+
+			this.categoryService.delete(category);
 			this.unauthenticate();
 			this.categoryService.flush();
 		} catch (final Throwable oops) {
