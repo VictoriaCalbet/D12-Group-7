4ebe36df8d03843788b2eb52009f7cb2ad098467
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-05-23 13:21:31 +0200

    Level C queries in repositories

--- Acme-Patronage/src/main/java/repositories/AnnouncementCommentRepository.java
+++ Acme-Patronage/src/main/java/repositories/AnnouncementCommentRepository.java
@@ -8,15 +8,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.AnnouncementComment;
-import domain.Comment;
 
 @Repository
 public interface AnnouncementCommentRepository extends JpaRepository<AnnouncementComment, Integer> {
 
 	@Query("select an.announcementComments from Announcement an where an.id=?1")
 	public Collection<AnnouncementComment> listAllAnnouncementComments(int announcementId);
-	
+
 	@Query("select an from AnnouncementComment an where an.user.id=?1")
 	public Collection<AnnouncementComment> listAllAnnouncementCommentsOfUser(int userId);
-	
+
 }
