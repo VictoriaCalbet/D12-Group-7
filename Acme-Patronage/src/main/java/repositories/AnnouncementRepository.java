2e4883685c74883673545c75cb7fa7393a642dc9
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-05-26 17:06:21 +0200

    Announcement stream query modified

--- Acme-Patronage/src/main/java/repositories/AnnouncementRepository.java
+++ Acme-Patronage/src/main/java/repositories/AnnouncementRepository.java
@@ -12,6 +12,9 @@ import domain.Announcement;
 @Repository
 public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
 
-	@Query("select ann from Announcement ann where ann.project in (select distinct(pat.project.id) from Patronage pat where pat.user.id = ?1 order by ann.creationMoment asc)")
+	//	@Query("select ann from Announcement ann where ann.project in (select distinct(pat.project.id) from Patronage pat where pat.user.id = ?1) order by ann.creationMoment desc")
+	//	Collection<Announcement> findStreamThatIFunded(int userId);
+
+	@Query("select a from Patronage pt join pt.project.announcements a where pt.user.id = ?1 order by a.creationMoment desc")
 	Collection<Announcement> findStreamThatIFunded(int userId);
 }
