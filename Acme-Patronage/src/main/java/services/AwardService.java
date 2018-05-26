90b974c4077e4caa0f42ae8945f1431e3a9ea20e
kain90 <utri1990@gmail.com>
2018-05-26 14:59:31 +0200

    Services - Award
    Añadidas comprobaciones de las fechas de dueDate para que no se creen premios después de la fecha dueDate: https://github.com/VictoriaCalbet/D12-Grupo-7/issues/17

--- Acme-Patronage/src/main/java/services/AwardService.java
+++ Acme-Patronage/src/main/java/services/AwardService.java
@@ -3,6 +3,7 @@ package services;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -82,8 +83,9 @@ public class AwardService {
 		Assert.notNull(award, "message.error.award.null");
 		Assert.notNull(user, "message.error.award.principal.null");
 		Assert.isTrue(award.getProject().getCreator().equals(user), "message.error.award.user.owner");
-		Assert.isTrue(award.getMoneyGoal() >= award.getProject().getMinimumPatronageAmount(), "message.error.award.moneyGoalIsLower");
 		Assert.isTrue(!award.getProject().getIsCancelled(), "message.error.award.project.isCancelled");
+		Assert.isTrue(award.getProject().getDueDate().after(new Date(System.currentTimeMillis() - 1000)), "message.error.award.dueDateIsPast");
+		Assert.isTrue(award.getMoneyGoal() >= award.getProject().getMinimumPatronageAmount(), "message.error.award.moneyGoalIsLower");
 
 		// Paso 1: realizo la entidad del servicio Award
 
@@ -107,7 +109,9 @@ public class AwardService {
 		Assert.isTrue(award.getProject().getCreator().equals(user), "message.error.award.user.owner");
 		Assert.isTrue(!award.getProject().getIsCancelled(), "message.error.award.project.isCancelled");
 		Assert.isTrue(award.getProject().getIsDraft(), "message.error.award.project.isPublished");
+		Assert.isTrue(award.getProject().getDueDate().after(new Date(System.currentTimeMillis() - 1000)), "message.error.award.dueDate");
 		Assert.isTrue(award.getMoneyGoal() >= award.getProject().getMinimumPatronageAmount(), "message.error.award.moneyGoalIsLower");
+
 		// Paso 1: realizo la entidad del servicio Award
 
 		result = this.save(award);
