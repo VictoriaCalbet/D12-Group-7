db4de3874e488a6afdf52370d11c93f698cdf299
kain90 <utri1990@gmail.com>
2018-05-27 22:36:38 +0200

    Tests - Announcement y Award

---
+++ Acme-Patronage/src/test/java/services/AnnouncementServiceTest.java
@@ -0,0 +1,213 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.Announcement;
+import domain.Project;
+import domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class AnnouncementServiceTest extends AbstractTest {
+
+	// The SUT (Service Under Test) -------------------------------------------
+
+	@Autowired
+	private AnnouncementService	announcementService;
+
+	@Autowired
+	private ProjectService		projectService;
+
+	@Autowired
+	private UserService			userService;
+
+
+	// Tests ------------------------------------------------------------------
+
+	/**
+	 * Acme-Patronage: Requirement 9.4
+	 * 
+	 * List the awards of every project if they have any
+	 * 
+	 * 
+	 * Positive test 1: An actor logged as user get project announcements
+	 * Positive test 2: An actor logged as moderator get project announcements
+	 * Positive test 3: An actor logged as admin get project announcements
+	 * Positive test 4: An actor logged as corporation get project announcements
+	 */
+
+	@Test
+	public void testListAnnouncementDriver() {
+		final Object[][] testingData = {
+			{
+				"user1", "project1", null
+			}, {
+				"moderator1", "project1", null
+			}, {
+				"admin", "project1", null
+			}, {
+				"corporation1", "project1", null
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testListAnnouncementsTemplate((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+
+	protected void testListAnnouncementsTemplate(final String actor, final String projectName, final Class<?> expectedException) {
+		Class<?> caught = null;
+
+		try {
+			this.authenticate(actor);
+
+			Collection<Announcement> announcements = null;
+			Project project = null;
+
+			project = this.projectService.findOne(this.getEntityId(projectName));
+			announcements = project.getAnnouncements();
+
+			Assert.isTrue(announcements.size() == 1);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 23.1
+	 * 
+	 * Post an announcement of a project he or she has created.
+	 * Announcements cannot be edited or deleted.
+	 * 
+	 * 
+	 * Positive test 1: An actor logged as user post an announcement.
+	 * Positive test 2: An actor logged as user post an announcement in project that it's not the creator.
+	 * Negative test 3: An actor logged as user post an announcement with null title
+	 * Negative test 4: An actor logged as user post an announcement with null description
+	 * Negative test 5: An actor logged as moderator post an announcement
+	 * Negative test 6: An actor logged as administrator post an announcement
+	 * Negative test 7: An actor logged as corporation post an announcement
+	 */
+
+	@Test
+	public void testPostAnAnnouncementDriver() {
+		final Object[][] testingData = {
+			{
+				"user1", "project1", "Announcement title", "AnnouncementDescription", null
+			}, {
+				"user1", "project2", "Announcement title", "AnnouncementDescription", IllegalArgumentException.class
+			}, {
+				"user1", "project1", null, "AnnouncementDescription", ConstraintViolationException.class
+			}, {
+				"user1", "project1", "Announcement title", null, ConstraintViolationException.class
+			}, {
+				"moderator1", "project1", "Announcement title", "AnnouncementDescription", IllegalArgumentException.class
+			}, {
+				"admin", "project1", "Announcement title", "Announcement description", IllegalArgumentException.class
+			}, {
+				"corporation1", "project1", "Announcement title", "AnnouncementDescription", IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testPostAnAnnouncementDriver((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (Class<?>) testingData[i][4]);
+	}
+	protected void testPostAnAnnouncementDriver(final String actor, final String projectName, final String announcementTitle, final String announcementDescription, final Class<?> expectedException) {
+		Class<?> caught = null;
+
+		try {
+			this.authenticate(actor);
+
+			User user = null;
+			Announcement announcement = null;
+			Project project = null;
+
+			user = this.userService.findByPrincipal();
+			announcement = this.announcementService.create(this.getEntityId(projectName));
+			project = this.projectService.findOne(this.getEntityId(projectName));
+
+			announcement.setTitle(announcementTitle);
+			announcement.setDescription(announcementDescription);
+			announcement.setUser(user);
+			announcement.setProject(project);
+
+			this.announcementService.saveFromCreate(announcement);
+			this.announcementService.flush();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 23.2
+	 * 
+	 * Display a stream of announcements regarding the projects that he or
+	 * she has funded.
+	 * 
+	 * Positive test 1: an user display his or her stream of announcements
+	 * Negative test 2: an moderator display a stream of announcements
+	 * Negative test 3: an admin display a stream of announcements
+	 * Negative test 4: an corporation display a stream of announcements
+	 */
+	@Test
+	public void testStreamOfAnnouncementDriver() {
+		final Object[][] testingData = {
+			{
+				"user1", "project1", null
+			}, {
+				"moderator1", "project1", IllegalArgumentException.class
+			}, {
+				"admin", "project1", IllegalArgumentException.class
+			}, {
+				"corporation1", "project1", IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testStreamOfAnnouncementsTemplate((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+	protected void testStreamOfAnnouncementsTemplate(final String actor, final String projectName, final Class<?> expectedException) {
+		Class<?> caught = null;
+
+		try {
+			this.authenticate(actor);
+
+			Collection<Announcement> announcements = null;
+			User user = null;
+
+			user = this.userService.findByPrincipal();
+			announcements = this.announcementService.findStreamThatIFunded(user.getId());
+
+			Assert.isTrue(announcements.size() == 2);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+}
---
+++ Acme-Patronage/src/test/java/services/AwardServiceTest.java
@@ -0,0 +1,282 @@
+
+package services;
+
+import java.util.Collection;
+
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.Award;
+import domain.Project;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@Transactional
+public class AwardServiceTest extends AbstractTest {
+
+	// The SUT (Service Under Test) -------------------------------------------
+
+	@Autowired
+	private AwardService	awardService;
+
+	@Autowired
+	private ProjectService	projectService;
+
+
+	// Tests ------------------------------------------------------------------
+
+	/**
+	 * Acme-Patronage: Requirement 11.4
+	 * 
+	 * Manage the awards he or she has created. The user is able to create, edit and
+	 * delete an award. Awards can be edited as long as its project is not published.
+	 * However, further awards can be added to the project even if it’s published.
+	 */
+
+	/**
+	 * 
+	 * Create an award.
+	 * 
+	 * Positive test 1: An actor logged as user create a award to his or her project
+	 * Negative test 2: An actor logged as user create a award with null name
+	 * Negative test 3: An actor logged as user create a award with null description
+	 * Negative test 4: An actor logged as user create a award with money goal too low
+	 * Negative test 5: An actor logged as user create a award that it's not the creator
+	 * Negative test 6: An actor logged as admin create a project
+	 * Negative test 7: An actor logged as moderator create a project
+	 * Negative test 8: An actor logged as corporation create a project
+	 */
+
+	@Test
+	public void testCreateAnAwardDriver() {
+		final Object[][] testingData = {
+			{
+				"user1", "project1", "Award name", "Award description", 60d, null
+			}, {
+				"user1", "project1", null, "Award description", 60d, ConstraintViolationException.class
+			}, {
+				"user1", "project1", "Award name", null, 60d, ConstraintViolationException.class
+			}, {
+				"user1", "project1", "Award name", "Award description", 0d, IllegalArgumentException.class
+			}, {
+				"user1", "project2", "Award name", "Award description", 60d, IllegalArgumentException.class
+			}, {
+				"admin", "project1", "Award name", "Award description", 60d, IllegalArgumentException.class
+			}, {
+				"moderator1", "project1", "Award name", "Award description", 60d, IllegalArgumentException.class
+			}, {
+				"corporation1", "project1", "Award name", "Award description", 60d, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testCreateAnAwardTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (double) testingData[i][4], (Class<?>) testingData[i][5]);
+	}
+
+	protected void testCreateAnAwardTemplate(final String actor, final String projectName, final String awardName, final String awardDescription, final double awardMoneyGoal, final Class<?> expectedException) {
+		Class<?> caught = null;
+
+		try {
+			this.authenticate(actor);
+
+			Award award = null;
+			Project project = null;
+
+			award = this.awardService.create(this.getEntityId(projectName));
+			project = this.projectService.findOne(this.getEntityId(projectName));
+
+			award.setName(awardName);
+			award.setDescription(awardDescription);
+			award.setMoneyGoal(awardMoneyGoal);
+			award.setProject(project);
+
+			this.awardService.saveFromCreate(award);
+			this.awardService.flush();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * 
+	 * Edit an award.
+	 * 
+	 * Positive test 1: An actor logged as user edit a award to his or her project
+	 * Negative test 2: An actor logged as user edit a award with null name
+	 * Negative test 3: An actor logged as user edit a award with null description
+	 * Negative test 4: An actor logged as user edit a award that it's not owner.
+	 */
+
+	@Test
+	public void testEditAnAwardDriver() {
+		final Object[][] testingData = {
+			{
+				"user3", "award5", "Award name modified", "Award description modified", 60d, null
+			}, {
+				"user3", "award5", null, "Award description modified", 60d, ConstraintViolationException.class
+			}, {
+				"user3", "award5", "Award name modified", null, 60d, ConstraintViolationException.class
+			}, {
+				"user1", "award5", "Award name modified", "Award description modified", 60d, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testEditAnAwardTemplate((String) testingData[i][0], (String) testingData[i][1], (String) testingData[i][2], (String) testingData[i][3], (double) testingData[i][4], (Class<?>) testingData[i][5]);
+	}
+
+	protected void testEditAnAwardTemplate(final String actor, final String awardEntity, final String awardNameModified, final String awardDescriptionModified, final double awardMoneyGoalModified, final Class<?> expectedException) {
+		Class<?> caught = null;
+
+		try {
+			this.authenticate(actor);
+
+			Award award = null;
+
+			award = this.awardService.findOne(this.getEntityId(awardEntity));
+
+			award.setName(awardNameModified);
+			award.setDescription(awardDescriptionModified);
+			award.setMoneyGoal(awardMoneyGoalModified);
+
+			this.awardService.saveFromEdit(award);
+			this.awardService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * 
+	 * Delete an award.
+	 * 
+	 * Positive test 1: An actor logged as user delete a award to his or her project
+	 * Negative test 2: An actor logged as user delete a award that it's not owner
+	 * Negative test 3: An actor logged as user delete a award with published project
+	 * Negative test 4: An actor logged as admin delete a award
+	 */
+
+	@Test
+	public void testDeleteAnAwardDriver() {
+		final Object[][] testingData = {
+			{
+				"user3", "award5", null
+			}, {
+				"user3", "award1", IllegalArgumentException.class
+			}, {
+				"user1", "award1", IllegalArgumentException.class
+			}, {
+				"user1", "award1", IllegalArgumentException.class
+			}, {
+				"admin", "award1", IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testDeleteAnAwardTemplate((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+
+	protected void testDeleteAnAwardTemplate(final String actor, final String awardEntity, final Class<?> expectedException) {
+		Class<?> caught = null;
+
+		try {
+			this.authenticate(actor);
+
+			Award award = null;
+
+			award = this.awardService.findOne(this.getEntityId(awardEntity));
+
+			this.awardService.delete(award);
+
+			this.awardService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 9.4
+	 * 
+	 * List the awards of every project if they have any.
+	 * 
+	 * Positive test 1: An actor logged as user list awards of a project
+	 * Positive test 2: An actor logged as moderator list awards of a project
+	 * Positive test 3: An actor logged as admin list awards of a project
+	 * Positive test 4: An actor logged as corporation list awards of a project
+	 * Negative test 5: An actor logged as user list awards of a cancelled project
+	 * Negative test 6: An actor logged as admin list awards of a cancelled project
+	 * Negative test 7: An actor logged as moderator list awards of a cancelled project
+	 * Negative test 8: An actor logged as corporation list awards of a cancelled project
+	 */
+
+	@Test
+	public void testListAwardsOfAProjectDriver() {
+		final Object[][] testingData = {
+			{
+				"user1", "project1", null
+			}, {
+				"moderator1", "project1", null
+			}, {
+				"admin", "project1", null
+			}, {
+				"corporation1", "project1", null
+			}, {
+				"user1", "project4", IllegalArgumentException.class
+			}, {
+				"admin", "project4", IllegalArgumentException.class
+			}, {
+				"moderator1", "project4", IllegalArgumentException.class
+			}, {
+				"corporation1", "project4", IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testListAwardsOfAProjectTemplate((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+
+	protected void testListAwardsOfAProjectTemplate(final String actor, final String projectName, final Class<?> expectedException) {
+		Class<?> caught = null;
+
+		try {
+			this.authenticate(actor);
+
+			Collection<Award> awards = null;
+			Project project = null;
+
+			project = this.projectService.findOne(this.getEntityId(projectName));
+			awards = project.getAwards();
+
+			Assert.isTrue(awards.size() == 1);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+}
