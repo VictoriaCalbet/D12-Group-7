8204fb12b75a09adc5ec408ebbd83e5b3f1d7ae2
kaze <meh@meh.meh>
2018-05-28 05:17:24 +0200

    Report and Sponsorship tests

---
+++ Acme-Patronage/src/test/java/services/ReportServiceTest.java
@@ -0,0 +1,289 @@
+
+package services;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.Report;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class ReportServiceTest extends AbstractTest {
+
+	@Autowired
+	private ReportService	reportService;
+
+	@Autowired
+	private ProjectService	projectService;
+
+
+	/**
+	 * Acme-Patronage: Requirement 31
+	 * 
+	 * An actor who is authenticated as a user must be able to:
+	 * Fill a complaint about any project.
+	 * 
+	 * 
+	 * 
+	 * Positive test1: User create report.
+	 * Negative test2: Moderator try to create a report.
+	 * Negative test3: Unaunthenticate actor try to create a report.
+	 */
+	@Test
+	public void testSaveFromCreateReport() {
+
+		//actor, exception.
+		final Object[][] testingData = {
+			{
+				"user1", null
+			}, {
+				"moderator1", IllegalArgumentException.class
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testSaveFromCreateReportTemplate((String) testingData[i][0], (Class<?>) testingData[i][1]);
+
+	}
+	protected void testSaveFromCreateReportTemplate(final String actor, final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+			this.authenticate(actor);
+			Report report;
+			report = this.reportService.create();
+			report.setComplaint("Complaint");
+			report.setProject(this.projectService.findOne(this.getEntityId("project1")));
+			this.reportService.saveFromCreate(report);
+			this.unauthenticate();
+			this.reportService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+	/**
+	 * Acme-Patronage: Requirement 32
+	 * 
+	 * An actor who is authenticated as a moderator must be able to:
+	 * Accept or reject a complaint
+	 * 
+	 * 
+	 * 
+	 * Positive test1: Moderator accept report.
+	 * Negative test2: User try to accept a report.
+	 * Negative test3: Unaunthenticate actor try to accept a report.
+	 */
+	@Test
+	public void testAcceptReport() {
+
+		//actor, exception.
+		final Object[][] testingData = {
+			{
+				"moderator1", null
+			}, {
+				"user1", IllegalArgumentException.class
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testAcceptReportTemplate((String) testingData[i][0], (Class<?>) testingData[i][1]);
+
+	}
+	protected void testAcceptReportTemplate(final String actor, final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+			this.authenticate("user1");
+			Report report;
+			report = this.reportService.create();
+			report.setComplaint("Complaint");
+			report.setProject(this.projectService.findOne(this.getEntityId("project1")));
+			report = this.reportService.saveFromCreate(report);
+			this.unauthenticate();
+
+			this.authenticate(actor);
+			this.reportService.acceptReport(report.getId(), "Reason");
+			this.unauthenticate();
+			this.reportService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 32
+	 * 
+	 * An actor who is authenticated as a moderator must be able to:
+	 * Accept or reject a complaint
+	 * 
+	 * 
+	 * 
+	 * Positive test1: Moderator reject report.
+	 * Negative test2: User try to reject a report.
+	 * Negative test3: Unaunthenticate actor try to reject a report.
+	 */
+	@Test
+	public void testRejectReport() {
+
+		//actor, exception.
+		final Object[][] testingData = {
+			{
+				"moderator1", null
+			}, {
+				"user1", IllegalArgumentException.class
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testAcceptReportTemplate((String) testingData[i][0], (Class<?>) testingData[i][1]);
+
+	}
+	protected void testRejectReportTemplate(final String actor, final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+			this.authenticate("user1");
+			Report report;
+			report = this.reportService.create();
+			report.setComplaint("Complaint");
+			report.setProject(this.projectService.findOne(this.getEntityId("project1")));
+			report = this.reportService.saveFromCreate(report);
+			this.unauthenticate();
+
+			this.authenticate(actor);
+			this.reportService.rejectReport(report.getId());
+			this.unauthenticate();
+			this.reportService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 32
+	 * 
+	 * An actor who is authenticated as a moderator must be able to:
+	 * Accept or reject a complaint
+	 * 
+	 * 
+	 * 
+	 * Positive test1: Moderator list reports to revised.
+	 * 
+	 */
+	@Test
+	public void testListUnrevisedReports() {
+
+		//Exception
+		final Object[][] testingData = {
+			{
+				null
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testListUnrevisedReportsTemplate((Class<?>) testingData[i][0]);
+
+	}
+	protected void testListUnrevisedReportsTemplate(final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+			this.authenticate("user1");
+			Report report;
+			report = this.reportService.create();
+			report.setComplaint("Complaint");
+			report.setProject(this.projectService.findOne(this.getEntityId("project1")));
+			report = this.reportService.saveFromCreate(report);
+			this.unauthenticate();
+
+			Assert.notNull(this.reportService.listUnrevisedReports().size() > 0);
+			this.reportService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 28
+	 * 
+	 * Users can fill a report about a project.
+	 * A report is a complaint about any aspect of the project.
+	 * Moderators will investigate and decide if the complaint is legit.
+	 * If so, it will be accepted and automatically delivered to the
+	 * administrator. The system must store the reason why the report
+	 * was accepted.
+	 * 
+	 * 
+	 * 
+	 * Positive test1: list reports accepted.
+	 * 
+	 */
+	@Test
+	public void testListAcceptedReports() {
+
+		//Exception
+		final Object[][] testingData = {
+			{
+				null
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testListAcceptedReportsTemplate((Class<?>) testingData[i][0]);
+
+	}
+
+	protected void testListAcceptedReportsTemplate(final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+			this.authenticate("user1");
+			Report report;
+			report = this.reportService.create();
+			report.setComplaint("Complaint");
+			report.setProject(this.projectService.findOne(this.getEntityId("project1")));
+			report = this.reportService.saveFromCreate(report);
+			this.unauthenticate();
+
+			this.authenticate("moderator1");
+			this.reportService.acceptReport(report.getId(), "Reason");
+			this.unauthenticate();
+
+			Assert.notNull(this.reportService.listAcceptedReports().size() > 0);
+			this.reportService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+}
---
+++ Acme-Patronage/src/test/java/services/SponsorshipServiceTest.java
@@ -0,0 +1,246 @@
+
+package services;
+
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolationException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.Project;
+import domain.Sponsorship;
+
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class SponsorshipServiceTest extends AbstractTest {
+
+	// System under test ------------------------------------------------------
+	@Autowired
+	private SponsorshipService	sponsorshipService;
+	@Autowired
+	private ProjectService		projectService;
+
+
+	// Tests ------------------------------------------------------------------
+
+	/**
+	 * Acme-Patronage: Requirement 24
+	 * 
+	 * An actor who is authenticated as a corporation must be able to:
+	 * Manage its sponsorships, thus create, edit and delete them.
+	 * 
+	 * 
+	 * 
+	 * Positive test1: Corporation create an sponsorship.
+	 * Negative test2: Corporation try to create an sponsorship with wrong url banner.
+	 * Negative test3: Unaunthenticate actor try to create an sponsorship.
+	 */
+	@Test
+	public void testSaveFromCreateSponsorship() {
+
+		//Sponsorship: bannerURL, actor, exception.
+		final Object[][] testingData = {
+			{
+				"http://www.banner.com", "corporation1", null
+			}, {
+				"banner", "corporation1", ConstraintViolationException.class
+			}, {
+				"http://www.banner.com", null, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testSaveFromCreateSponsorshipTemplate((String) testingData[i][0], (String) testingData[i][1], (Class<?>) testingData[i][2]);
+
+	}
+	protected void testSaveFromCreateSponsorshipTemplate(final String bannerURL, final String actor, final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+
+			this.authenticate(actor);
+			final Sponsorship sponsorship;
+			sponsorship = this.sponsorshipService.create();
+			final Project project;
+			project = this.projectService.findOne(this.getEntityId("project1"));
+			sponsorship.setProject(project);
+			sponsorship.setBannerURL(bannerURL);
+			sponsorship.setPageURL("http://www.page.com");
+			this.sponsorshipService.saveFromCreate(sponsorship);
+			this.unauthenticate();
+			this.sponsorshipService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 24
+	 * 
+	 * An actor who is authenticated as a corporation must be able to:
+	 * Manage its sponsorships, thus create, edit and delete them.
+	 * 
+	 * 
+	 * 
+	 * Positive test1: Corporation1 edit an sponsorship.
+	 * Negative test2: Corporation2 try to edit an sponsorship.
+	 * Negative test3: Unaunthenticate actor try to edit an sponsorship.
+	 */
+	@Test
+	public void testSaveFromEditSponsorship() {
+
+		//Sponsorship: actor, exception.
+		final Object[][] testingData = {
+			{
+				"corporation1", null
+			}, {
+				"corporation2", IllegalArgumentException.class
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testSaveFromEditSponsorshipTemplate((String) testingData[i][0], (Class<?>) testingData[i][1]);
+
+	}
+	protected void testSaveFromEditSponsorshipTemplate(final String actor, final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+
+			this.authenticate("corporation1");
+			Sponsorship sponsorship;
+			sponsorship = this.sponsorshipService.create();
+			Project project;
+			project = this.projectService.findOne(this.getEntityId("project1"));
+			sponsorship.setProject(project);
+			sponsorship.setBannerURL("http://www.banner.com");
+			sponsorship.setPageURL("http://www.page.com");
+			sponsorship = this.sponsorshipService.saveFromCreate(sponsorship);
+			this.unauthenticate();
+
+			this.authenticate(actor);
+			sponsorship.setBannerURL("http://www.image.com");
+			this.sponsorshipService.saveFromEdit(sponsorship);
+			this.unauthenticate();
+
+			this.sponsorshipService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 24
+	 * 
+	 * An actor who is authenticated as a corporation must be able to:
+	 * Manage its sponsorships, thus create, edit and delete them.
+	 * 
+	 * 
+	 * 
+	 * Positive test1: Corporation remove an sponsorship.
+	 * Negative test2: User try to remove an sponsorship.
+	 * Negative test3: Unaunthenticate actor try to remove an sponsorship.
+	 */
+	@Test
+	public void testDeleteByCorporation() {
+
+		//Sponsorship: actor, exception.
+		final Object[][] testingData = {
+			{
+				"corporation1", null
+			}, {
+				"user1", IllegalArgumentException.class
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testDeleteByCorporationTemplate((String) testingData[i][0], (Class<?>) testingData[i][1]);
+
+	}
+	protected void testDeleteByCorporationTemplate(final String actor, final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+
+			this.authenticate("corporation1");
+			Sponsorship sponsorship;
+			sponsorship = this.sponsorshipService.create();
+			Project project;
+			project = this.projectService.findOne(this.getEntityId("project1"));
+			sponsorship.setProject(project);
+			sponsorship.setBannerURL("http://www.banner.com");
+			sponsorship.setPageURL("http://www.page.com");
+			sponsorship = this.sponsorshipService.saveFromCreate(sponsorship);
+			this.unauthenticate();
+
+			this.authenticate(actor);
+			this.sponsorshipService.deleteByCorporation(sponsorship);
+			this.unauthenticate();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+
+	/**
+	 * Acme-Patronage: Requirement 26
+	 * 
+	 * Whenever a project is displayed, a random sponsorship must be
+	 * selected and its banner shown, if any.
+	 * 
+	 * 
+	 * Positive test1: Display sponsorship.
+	 * 
+	 */
+
+	@Test
+	public void testRandomSponsorshipByProjectId() {
+
+		//Exception
+		final Object[][] testingData = {
+			{
+				null
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.testRandomSponsorshipByProjectIdTemplate((Class<?>) testingData[i][0]);
+
+	}
+	protected void testRandomSponsorshipByProjectIdTemplate(final Class<?> expectedException) {
+		Class<?> caught = null;
+		try {
+
+			Assert.notNull(this.sponsorshipService.getRandomSponsorshipByProjectId(this.getEntityId("project1")));
+			this.sponsorshipService.flush();
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.unauthenticate();
+		}
+
+		this.checkExceptions(expectedException, caught);
+	}
+}
